219596d381ebe286528a966ffd13e788fb0c5226
saowang <saowang@outlook.com>
2020-08-23 01:24:35 +0800

    Add Service Worker supported

---
+++ assets/js/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response !== undefined) {
+                return response;
+            } else {
+                return fetch(event.request).then(function(response) {
+                    let resp = response.clone();
+                    caches.open("sw").then(function(cache) {
+                        cache.put(event.request, resp);
+                    });
+                    return response;
+                }).catch(console.log);
+            }
+        })
+    );
+});
