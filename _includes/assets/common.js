4b7da2a00368d593554462c4d5a5a25523ca8fc2
saowang <saowang@outlook.com>
2020-08-22 08:59:35 +0800

    Move analytics codes to addons

--- _includes/assets/common.js
+++ _includes/assets/common.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
-    let analytics = new URL("https://rundocs-analytics.glitch.me/collect?v={{ version }}&lang={{ lang }}");
     let highlight = new URL(location.href).searchParams.get("highlight");
-    let current = $(".wy-menu-vertical").find(`[href="${location.pathname}"]`);
+    let link = $(".wy-menu-vertical").find(`[href="${location.pathname}"]`);
 
     /* directory toc */
-    if (current.length > 0) {
-        current.closest("li.toctree-l1").parent().addClass("current");
-        current.closest("li.toctree-l1").addClass("current");
-        current.closest("li.toctree-l2").addClass("current");
-        current.closest("li.toctree-l3").addClass("current");
-        current.closest("li.toctree-l4").addClass("current");
-        current.closest("li.toctree-l5").addClass("current");
-        current[0].scrollIntoView();
+    if (link.length > 0) {
+        link.closest("li.toctree-l1").parent().addClass("current");
+        link.closest("li.toctree-l1").addClass("current");
+        link.closest("li.toctree-l2").addClass("current");
+        link.closest("li.toctree-l3").addClass("current");
+        link.closest("li.toctree-l4").addClass("current");
+        link.closest("li.toctree-l5").addClass("current");
+        link[0].scrollIntoView();
     }
     /* content toc */
     $(".wy-menu-vertical li.current").append(function() {
         let level = parseInt($(this).attr("class").match(/toctree-l(\d+)/)[1]) + 1;
         let toc = ["<ul>"];
-        let sort = parseInt(this.dataset.sort);
+        /* let sort = parseInt(this.dataset.sort); */
 
         $(".document").find("h2,h3,h4,h5,h6").each(function() {
             toc.push(`<li class="toctree-l${level}"><a class="reference internal" href="#${this.id}">${$(this).text()}</a></li>`);
@@ -50,16 +49,12 @@ $(document).ready(function() {
             }
         });
     }
+
+    /* anchors */
+    anchors.add();
+
     /* admonition */
     $(".admonition-title").each(function() {
         $(this).html(ui.admonition[$(this).attr("ui")]);
     });
-    /* anchors */
-    anchors.add();
-
-    /* analytics */
-    analytics.searchParams.append("user_lang", navigator.language);
-    analytics.searchParams.append("host", location.host);
-    analytics.searchParams.append("platform", navigator.platform);
-    $.getJSON(analytics.toString(), (data) => $("#counter").html(data.count));
 });
