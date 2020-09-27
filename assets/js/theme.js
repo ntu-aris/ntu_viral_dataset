d76a25123531a9b41dfae3138805f0244b7925b0
saowang <saowang@outlook.com>
2020-09-27 15:58:42 +0800

    Adjust variable names

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,224 +1,211 @@
-$(document).ready(function () {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register(`${ui.baseurl}/sw.caches.js`);
-  } else {
-    debug("Service Worker not supported!");
-  }
+function search(data) {
+  let text = new URL(location.href).searchParams.get("q");
+  let lang = new URL(location.href).searchParams.get("lang") || ui.lang;
 
-  function debug() {
-    console.debug.apply(console, arguments);
-  }
-  let analytics = new URL(
-    `https://rundocs-analytics.glitch.me/collect?v=${ui.version}&lang=${ui.lang}`
-  );
-  analytics.searchParams.append("user_lang", navigator.language);
-  analytics.searchParams.append("host", location.host);
-  analytics.searchParams.append("platform", navigator.platform);
-  $.getJSON(analytics.toString(), (data) => $("#counter").html(data.count));
+  $("input[name='q']").val(text);
 
-  function search(data) {
-    let text = new URL(location.href).searchParams.get("q");
-    let lang = new URL(location.href).searchParams.get("lang") || ui.lang;
-
-    $("input[name='q']").val(text);
-
-    let results = [];
-    let regexp = new RegExp();
-    try {
-      regexp = new RegExp(text, "im");
-    } catch (e) {
-      $(".search-results .content").empty();
-      $(".search-results .summary").html(ui.i18n.search_results_not_found);
-      $(".search-results h2").html(ui.i18n.search_results);
-      return debug(e.message);
-    }
-
-    function slice(content, min, max) {
-      return content
-        .slice(min, max)
-        .replace(
-          regexp,
-          (match) => `<span class="highlighted">${match}</span>`
-        );
-    }
-    for (page of data) {
-      let [title, content] = [null, null];
-      try {
-        if (page.title) {
-          title = page.title.match(regexp);
-        } else {
-          if (page.url == "/") {
-            page.title = ui.title;
-          } else {
-            page.title = page.url;
-          }
-        }
-      } catch (e) {
-        debug(e.message);
-      }
-      try {
-        if (page.content) {
-          page.content = $("<div/>").html(page.content).text();
-          content = page.content.match(regexp);
-        }
-      } catch (e) {
-        debug(e.message);
-      }
-      if (title || content) {
-        let result = [
-          `<a href="${ui.baseurl}${page.url}?highlight=${text}">${page.title}</a>`,
-        ];
-        if (content) {
-          let [min, max] = [content.index - 100, content.index + 100];
-          let [prefix, suffix] = ["...", "..."];
-
-          if (min < 0) {
-            prefix = "";
-            min = 0;
-          }
-          if (max > page.content.length) {
-            suffix = "";
-            max = page.content.length;
-          }
-          result.push(
-            `<p class="text-gray">${prefix}${slice(
-              page.content,
-              min,
-              max
-            )}${suffix}</p>`
-          );
-        }
-        results.push(`<li class="border-top py-4">${result.join("")}</li>`);
-      }
-    }
-    if (results.length > 0 && text.length > 0) {
-      $(".search-results .content").html(results.join(""));
-      $(".search-results .summary").html(
-        ui.i18n.search_results_found.replace("#", results.length)
-      );
-    } else {
-      $(".search-results .content").empty();
-      $(".search-results .summary").html(ui.i18n.search_results_not_found);
-    }
+  let results = [];
+  let regexp = new RegExp();
+  try {
+    regexp = new RegExp(text, "im");
+  } catch (e) {
+    $(".search-results .content").empty();
+    $(".search-results .summary").html(ui.i18n.search_results_not_found);
     $(".search-results h2").html(ui.i18n.search_results);
+    return debug(e.message);
   }
 
-  function initialize(name) {
-    let link = $(".toctree").find(`[href="${decodeURI(name)}"]`);
-
-    if (link.length > 0) {
-      $(".toctree .current").removeClass("current");
-      link.addClass("current");
-      link.closest(".level-1").parent().addClass("current");
-      for (let i = 1; i <= 11; i++) {
-        link.closest(`.level-${i}`).addClass("current");
+  function slice(content, min, max) {
+    return content
+      .slice(min, max)
+      .replace(regexp, (match) => `<span class="highlighted">${match}</span>`);
+  }
+  for (page of data) {
+    let [title, content] = [null, null];
+    try {
+      if (page.title) {
+        title = page.title.match(regexp);
+      } else {
+        if (page.url == "/") {
+          page.title = ui.title;
+        } else {
+          page.title = page.url;
+        }
       }
+    } catch (e) {
+      debug(e.message);
     }
-    // if(/^#/.test(name) && $(name).length >0){
-    //     $(name)[0].scrollIntoView();
-    // }
+    try {
+      if (page.content) {
+        page.content = $("<div/>").html(page.content).text();
+        content = page.content.match(regexp);
+      }
+    } catch (e) {
+      debug(e.message);
+    }
+    if (title || content) {
+      let result = [
+        `<a href="${ui.baseurl}${page.url}?highlight=${text}">${page.title}</a>`,
+      ];
+      if (content) {
+        let [min, max] = [content.index - 100, content.index + 100];
+        let [prefix, suffix] = ["...", "..."];
+
+        if (min < 0) {
+          prefix = "";
+          min = 0;
+        }
+        if (max > page.content.length) {
+          suffix = "";
+          max = page.content.length;
+        }
+        result.push(
+          `<p class="text-gray">${prefix}${slice(
+            page.content,
+            min,
+            max
+          )}${suffix}</p>`
+        );
+      }
+      results.push(`<li class="border-top py-4">${result.join("")}</li>`);
+    }
   }
-
-  function toggleCurrent(link) {
-    let closest = link.closest("li");
-    closest.siblings("li.current").removeClass("current");
-    closest.siblings().find("li.current").removeClass("current");
-    closest.find("> ul li.current").removeClass("current");
-    closest.toggleClass("current");
+  if (results.length > 0 && text.length > 0) {
+    $(".search-results .content").html(results.join(""));
+    $(".search-results .summary").html(
+      ui.i18n.search_results_found.replace("#", results.length)
+    );
+  } else {
+    $(".search-results .content").empty();
+    $(".search-results .summary").html(ui.i18n.search_results_not_found);
   }
+  $(".search-results h2").html(ui.i18n.search_results);
+}
 
-  function toc() {
-    $(".toctree li.current")
-      .append('<ul class="content-toc"></ul>')
-      .html(function () {
-        let level = parseInt(this.dataset.level);
-        let temp = 0;
-        let stack = [$(this).find(".content-toc")];
+function initialize(name) {
+  let link = $(".toctree").find(`[href="${decodeURI(name)}"]`);
 
-        $(".markdown-body")
-          .find("h2,h3,h4,h5,h6")
-          .each(function () {
-            let anchor = $("<a/>")
-              .addClass("d-flex flex-items-baseline")
-              .text($(this).text())
-              .attr("href", `#${this.id}`);
-            let tagLevel = parseInt(this.tagName.slice(1)) - 1;
+  if (link.length > 0) {
+    $(".toctree .current").removeClass("current");
+    link.addClass("current");
+    link.closest(".level-1").parent().addClass("current");
+    for (let i = 1; i <= 11; i++) {
+      link.closest(`.level-${i}`).addClass("current");
+    }
+  }
+}
 
-            if (tagLevel > temp) {
-              let parent = stack[0].children("li:last")[0];
-              if (parent) {
-                stack.unshift($("<ul/>").appendTo(parent));
-              }
-            } else {
-              stack.splice(
-                0,
-                Math.min(temp - tagLevel, Math.max(stack.length - 1, 0))
-              );
+function toggleCurrent(link) {
+  let closest = link.closest("li");
+  closest.siblings("li.current").removeClass("current");
+  closest.siblings().find("li.current").removeClass("current");
+  closest.find("> ul li.current").removeClass("current");
+  closest.toggleClass("current");
+}
+
+function toc() {
+  $(".toctree li.current")
+    .append('<ul class="content-toc"></ul>')
+    .html(function () {
+      let level = parseInt(this.dataset.level);
+      let temp = 0;
+      let stack = [$(this).find(".content-toc")];
+
+      $(".markdown-body")
+        .find("h2,h3,h4,h5,h6")
+        .each(function () {
+          let anchor = $("<a/>")
+            .addClass("d-flex flex-items-baseline")
+            .text($(this).text())
+            .attr("href", `#${this.id}`);
+          let tagLevel = parseInt(this.tagName.slice(1)) - 1;
+
+          if (tagLevel > temp) {
+            let parent = stack[0].children("li:last")[0];
+            if (parent) {
+              stack.unshift($("<ul/>").appendTo(parent));
             }
-            temp = tagLevel;
+          } else {
+            stack.splice(
+              0,
+              Math.min(temp - tagLevel, Math.max(stack.length - 1, 0))
+            );
+          }
+          temp = tagLevel;
 
-            $("<li/>")
-              .addClass(`toc level-${level + tagLevel}`)
-              .append(anchor)
-              .appendTo(stack[0]);
-          });
-        if (!stack[0].html()) {
-          stack[0].remove();
+          $("<li/>")
+            .addClass(`toc level-${level + tagLevel}`)
+            .append(anchor)
+            .appendTo(stack[0]);
+        });
+      if (!stack[0].html()) {
+        stack[0].remove();
+      }
+    });
+}
+
+function set(name, value) {
+  return localStorage.setItem(name, value);
+}
+
+function get(name) {
+  return localStorage.getItem(name) || false;
+}
+
+function debug() {
+  console.debug.apply(console, arguments);
+}
+
+function restore() {
+  let scroll = get("scroll");
+  let scrollTime = get("scrollTime");
+  let scrollHost = get("scrollHost");
+
+  if (scroll && scrollTime && scrollHost) {
+    if (scrollHost == location.host && Date.now() - scrollTime < 6e5) {
+      $(".sidebar").scrollTop(scroll);
+    }
+  }
+  $(".sidebar").scroll(function () {
+    set("scroll", this.scrollTop);
+    set("scrollTime", Date.now());
+    set("scrollHost", location.host);
+  });
+}
+
+function highlight() {
+  let text = new URL(location.href).searchParams.get("highlight");
+  let box = ".highlighted-box";
+
+  if (text) {
+    $(".markdown-body")
+      .find("*")
+      .each(function () {
+        try {
+          if (this.outerHTML.match(new RegExp(text, "im"))) {
+            $(this).addClass("highlighted-box");
+          }
+        } catch (e) {
+          debug(e.message);
+        }
+      });
+    $(".markdown-body")
+      .find(box)
+      .each(function () {
+        if ($(this).find(box).length > 0) {
+          $(this).removeClass(box);
         }
       });
   }
+}
 
-  function set(name, value) {
-    return localStorage.setItem(name, value);
-  }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register(`${ui.baseurl}/sw.caches.js`);
+} else {
+  debug("Service Worker not supported!");
+}
 
-  function get(name) {
-    return localStorage.getItem(name) || false;
-  }
-
-  function restore() {
-    let scroll = get("scroll");
-    let scrollTime = get("scrollTime");
-    let scrollHost = get("scrollHost");
-
-    if (scroll && scrollTime && scrollHost) {
-      if (scrollHost == location.host && Date.now() - scrollTime < 6e5) {
-        $(".sidebar").scrollTop(scroll);
-      }
-    }
-    $(".sidebar").scroll(function () {
-      set("scroll", this.scrollTop);
-      set("scrollTime", Date.now());
-      set("scrollHost", location.host);
-    });
-  }
-
-  function highlight() {
-    let text = new URL(location.href).searchParams.get("highlight");
-    let box = ".highlighted-box";
-
-    if (text) {
-      $(".markdown-body")
-        .find("*")
-        .each(function () {
-          try {
-            if (this.outerHTML.match(new RegExp(text, "im"))) {
-              $(this).addClass("highlighted-box");
-            }
-          } catch (e) {
-            debug(e.message);
-          }
-        });
-      $(".markdown-body")
-        .find(box)
-        .each(function () {
-          if ($(this).find(box).length > 0) {
-            $(this).removeClass(box);
-          }
-        });
-    }
-  }
-
+$(document).ready(function () {
   if (location.pathname == `${ui.baseurl}/search.html`) {
     $.ajax(`${ui.baseurl}/pages.json`)
       .done(search)
@@ -286,4 +273,9 @@ $(document).ready(function () {
   $(window).bind("hashchange", () =>
     initialize(location.hash || location.pathname)
   );
+
+  ui.collect.searchParams.append("host", location.host);
+  ui.collect.searchParams.append("user_lang", navigator.language);
+  ui.collect.searchParams.append("platform", navigator.platform);
+  $.getJSON(ui.collect.toString(), (data) => $("#counter").html(data.count));
 });
